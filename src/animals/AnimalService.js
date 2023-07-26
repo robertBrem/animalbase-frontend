f6c1675492b724b29f615637913cd7081a14ab20
Robert Brem <rbrem@confirm.ch>
2023-07-26 18:33:07 +0200

    FEATURE: Add POST

--- src/animals/AnimalService.js
+++ src/animals/AnimalService.js
@@ -1,7 +1,23 @@
 export default class AnimalService {
+    url = `http://localhost`;
+
     async getAll() {
-        return fetch(`http://localhost:8090/api/animals`, {cache: 'reload'})
+        return fetch(`${this.url}:8090/api/animals`, {cache: 'reload'})
             .then(result => result.json())
+            .catch(error => {
+                console.log(error)
+            });
+    }
+
+    async post(object) {
+        return fetch(`${this.url}:8090/api/animals`, {
+            method: 'POST',
+            cache: 'reload',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(object)
+        })
             .catch(error => {
                 console.log(error)
             });
--- src/animals/AnimalsView.js
+++ src/animals/AnimalsView.js
@@ -6,43 +6,62 @@ export default class AnimalsView extends HTMLElement {
         super();
         this.root = this.attachShadow({mode: 'open'});
     }
+
     connectedCallback() {
-        new AnimalService().getAll()
+        this.service = new AnimalService()
+        this.service.getAll()
             .then(animals => {
                 this.animals = animals;
                 render(this.getTemplate(), this.root)
             });
     }
 
+    handleAddAnimal() {
+        this.service
+            .post({
+                "names": [
+                    {"language": "de", "translation": "Spaltenschildkröte"},
+                    {"language": "en", "translation": "Pancake Tortoise"}],
+                "imagePaths": [
+                    {"path": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Malacochersus_tornieri_-_Buffalo_Zoo.jpg/220px-Malacochersus_tornieri_-_Buffalo_Zoo.jpg"}
+                ],
+                "chipId": "alsdkjfl;asdf",
+                "birthday": "2023-05-01",
+                "acquired": "2023-05-01"
+            })
+            .then(render(this.getTemplate(), this.root))
+    }
+
     getTemplate() {
         let animalEntries = this.animals
-            .map(animal => html`<tr>
-            <td>${animal.chipId}</td>
-            <td><img src="${animal.imagePaths}" /></td>
-            <td>${animal.names[0].translation}</td>
-            <td>${animal.birthday}</td>
-            <td>${animal.acquired}</td>
-        </tr>`);
+            .map(animal => html`
+                <tr>
+                    <td>${animal.chipId}</td>
+                    <td><img src="${animal.imagePaths[0].path}"/></td>
+                    <td>${animal.names[0].translation}</td>
+                    <td>${animal.birthday}</td>
+                    <td>${animal.acquired}</td>
+                </tr>`);
         console.log(this.animals)
         return html`
             <h1>Animals</h1>
-<h2>Reptilien</h2>
-<h3>Schildkröten</h3>
-<h4>Spaltenschildkröte</h4>
-<table>
-    <thead>
-        <tr>
-            <th>Chip ID</th>
-            <th>BILD</th>
-            <th>Name</th>
-            <th>Alter</th>
-            <th>Hier seit</th>
-        </tr>
-    </thead>
-    <tbody>
-        ${animalEntries}
-    </tbody>
-</table>
+            <h2>Reptilien</h2>
+            <h3>Schildkröten</h3>
+            <h4 @click="${_ => this.handleAddAnimal()}">Spaltenschildkröte</h4>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Chip ID</th>
+                        <th>BILD</th>
+                        <th>Name</th>
+                        <th>Alter</th>
+                        <th>Hier seit</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    ${animalEntries}
+                    </tbody>
+                </table>
         `
     }
 
